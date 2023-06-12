2f6689037ee1bed3a4ca70b47cd9ccad8457e553
samuelcosta25 <samu.live@outlook.com>
2023-06-12 00:49:58 -0300

    12.06.2023

---
+++ Site/script.js
@@ -0,0 +1,40 @@
+function cadastrar() {
+    let check=false;
+
+    if (document.getElementById("checkbox").checked){
+        check=true
+    }
+
+    if (document.getElementById("inputEmail").value == ''
+        || document.getElementById("inputPassword").value == ''
+        || document.getElementById("inputAddress").value == ''
+        || document.getElementById("inputCity").value == ''
+        || check==false) {
+        alert("Preencha todos os campos!");
+    }
+
+}
+function habilitaOption() {
+    if (document.getElementById("inputEmail").value != ''
+        && document.getElementById("inputPassword").value != ''
+        && document.getElementById("inputAddress").value != ''
+        && document.getElementById("inputCity").value != '') {
+        document.getElementById("estado").removeAttribute("disabled");
+        document.getElementById("stranger").removeAttribute("disabled");
+    }
+
+}
+
+function entrar() {
+    let check=false;
+
+    if (document.getElementById("checkRobot").checked){
+        check=true
+    }
+
+    if (document.getElementById("email").value == ''
+        || document.getElementById("password").value == ''
+        || check==false) {
+        alert("Preencha todos os campos!");
+    }
+}
